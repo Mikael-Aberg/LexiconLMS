a4acd23306238142ce314d5c131791279082c280
Mikael Åberg <mikael.aberg@me.com>
2017-03-20 09:34:09 +0100

    Add path to document model

---
+++ LexiconLMS/Models/Activity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LexiconLMS.Models
+{
+    public class Activity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        public virtual Module Moduel { get; set; }
+        public virtual ActivityType Type { get; set; }
+        public virtual ICollection<Document> Documents { get; set; }
+    }
+}
---
+++ LexiconLMS/Models/Document.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LexiconLMS.Models
+{
+    public class Document
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime UpploadTime { get; set; }
+        public bool Shared { get; set; }
+        public string FilePath { get; set; }
+
+
+        public virtual ApplicationUser User { get; set; }
+    }
+}
