0d48fb0fa4c8d8ae96b3146cefe595e240a7e3cf
Mikael Åberg <mikael.aberg@me.com>
2017-03-21 10:43:01 +0100

    Add DisplayName to properties in activity and module

--- LexiconLMS/Models/Activity.cs
+++ LexiconLMS/Models/Activity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -8,12 +9,17 @@ namespace LexiconLMS.Models
     public class Activity
     {
         public int Id { get; set; }
+        [DisplayName("Namn")]
         public string Name { get; set; }
+        [DisplayName("Beskrivning")]
         public string Description { get; set; }
+        [DisplayName("Start tid")]
         public DateTime StartTime { get; set; }
+        [DisplayName("Slut tid")]
         public DateTime EndTime { get; set; }
 
         public virtual Module Moduel { get; set; }
+        [DisplayName("Typ")]
         public virtual ActivityType Type { get; set; }
         public virtual ICollection<Document> Documents { get; set; }
     }
