325e82d62c9749268bce380545c00a8826c2bc5d
Mikael Åberg <mikael.aberg@me.com>
2017-03-21 14:17:19 +0100

    Add view to add and change activities

--- LexiconLMS/Models/Activity.cs
+++ LexiconLMS/Models/Activity.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -10,18 +11,27 @@ namespace LexiconLMS.Models
     public class Activity
     {
         public int Id { get; set; }
+
         [DisplayName("Namn")]
+        [Required]
         public string Name { get; set; }
         [DisplayName("Beskrivning")]
         public string Description { get; set; }
         [DisplayName("Start tid")]
+        [Required]
         [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd HH:mm}", ApplyFormatInEditMode = true)]
         public DateTime StartTime { get; set; }
         [DisplayName("Slut tid")]
+        [Required]
         [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd HH:mm}", ApplyFormatInEditMode = true)]
         public DateTime EndTime { get; set; }
 
+        public int ModuelId { get; set; }
+        public int TypeId { get; set; }
+
+        [ForeignKey("ModuelId")]
         public virtual Module Moduel { get; set; }
+        [ForeignKey("TypeId")]
         [DisplayName("Typ")]
         public virtual ActivityType Type { get; set; }
         public virtual ICollection<Document> Documents { get; set; }
