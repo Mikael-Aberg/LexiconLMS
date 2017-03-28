0d27aef0a056248b50e10c66e317ace71dc71881
Mikael Åberg <mikael.aberg@me.com>
2017-03-28 11:53:16 +0200

    Add start of student splash screen

---
+++ LexiconLMS/Models/StudentHomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LexiconLMS.Models
+{
+    public class StudentHomeViewModel
+    {
+        public int CourseId { get; set; }
+        public Module Module { get; set; }
+    }
+}
