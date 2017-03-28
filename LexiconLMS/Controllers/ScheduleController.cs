06e93bcbf083f0714ec03278c54eeeebdb31c69a
Mikael Åberg <mikael.aberg@me.com>
2017-03-28 10:11:27 +0200

    Add ability to see current weeks schedule

--- LexiconLMS/Controllers/ScheduleController.cs
+++ LexiconLMS/Controllers/ScheduleController.cs
@@ -1,6 +1,7 @@
 ﻿using LexiconLMS.Models;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Web;
@@ -13,7 +14,7 @@ namespace LexiconLMS.Controllers
         private ApplicationDbContext db = new ApplicationDbContext();
 
         // GET: Schedule
-        public ActionResult Index(int? id)
+        public ActionResult Index(int? id, DateTime? weekDay)
         {
             if (id == null)
             {
@@ -22,7 +23,7 @@ namespace LexiconLMS.Controllers
 
             var course = db.Courses.Find(id);
 
-            if(course == null)
+            if (course == null)
             {
                 return RedirectToAction("Index", "Courses", null);
             }
@@ -33,15 +34,30 @@ namespace LexiconLMS.Controllers
             TimeSpan afternoonStart = new TimeSpan(12, 0, 0);
             TimeSpan afternoonEnd = new TimeSpan(17, 0, 0);
 
+            DateTime startDate;
+            DateTime endDate;
+
+            if (weekDay != null)
+            {
+                var week = GetWeek((DateTime)weekDay).OrderBy(x => x.Date).ToList();
+                startDate = week.First().Date + new TimeSpan(0,0,0);
+                endDate = week.Last().Date + new TimeSpan(0, 0, 0);
+            }
+            else
+            {
+                startDate = course.StartDate.Date + new TimeSpan(0, 0, 0);
+                endDate = course.EndDate.Date + new TimeSpan(0, 0, 0);
+            }
+
             var scheduleList = new List<SchedulePost>();
-            for (DateTime date = course.StartDate; date <= course.EndDate; date = date.AddDays(1))
+            for (DateTime date = startDate; date <= endDate; date = date.AddDays(1))
             {
                 var post = new SchedulePost();
                 post.Morning = new List<ScheduleLink>();
                 post.Afternoon = new List<ScheduleLink>();
                 post.Date = date.ToShortDateString();
-                post.Day = date.DayOfWeek.ToString();
-                var modules = course.Modules.Where(x => (x.StartDate <= date && x.EndDate >= date));
+                post.Day = GetSwedishDay(date.DayOfWeek);
+                var modules = course.Modules.Where(x => (x.StartDate.Date <= date && x.EndDate.Date >= date));
                 StringBuilder moduleBuilder = new StringBuilder();
                 foreach (var module in modules)
                 {
@@ -72,8 +88,87 @@ namespace LexiconLMS.Controllers
 
                 scheduleList.Add(post);
             }
-            var viewModel = new ScheduleViewModel { Name = course.Name, Schedule = scheduleList};
+            var viewModel = new ScheduleViewModel { Name = course.Name, Schedule = scheduleList };
             return View(viewModel);
+        }
+
+        private string GetSwedishDay(DayOfWeek day)
+        {
+            switch (day)
+            {
+                case DayOfWeek.Monday:
+                    return "Måndag";
+                case DayOfWeek.Tuesday:
+                    return "Tisdag";
+                case DayOfWeek.Wednesday:
+                    return "Onsdag";
+                case DayOfWeek.Thursday:
+                    return "Torsdag";
+                case DayOfWeek.Friday:
+                    return "Fredag";
+                case DayOfWeek.Saturday:
+                    return "Lördag";
+                case DayOfWeek.Sunday:
+                    return "Söndag";
+            }
+            return null;
+        }
+
+        private List<DateTime> GetWeek(DateTime date)
+        {
+            var week = new List<DateTime>();
+            DateTime startDate;
+            DateTime endDate;
+
+            switch (date.DayOfWeek)
+            {
+                case DayOfWeek.Monday:
+                    startDate = date;
+                    endDate = date.AddDays(6);
+                    break;
+
+                case DayOfWeek.Tuesday:
+                    startDate = date.AddDays(-1);
+                    endDate = date.AddDays(5);
+                    break;
+
+                case DayOfWeek.Wednesday:
+                    startDate = date.AddDays(-2);
+                    endDate = date.AddDays(4);
+                    break;
+
+                case DayOfWeek.Thursday:
+                    startDate = date.AddDays(-3);
+                    endDate = date.AddDays(3);
+                    break;
+
+                case DayOfWeek.Friday:
+                    startDate = date.AddDays(-4);
+                    endDate = date.AddDays(2);
+                    break;
+
+                case DayOfWeek.Saturday:
+                    startDate = date.AddDays(-5);
+                    endDate = date.AddDays(1);
+                    break;
+
+                case DayOfWeek.Sunday:
+                    startDate = date.AddDays(-6);
+                    endDate = date;
+                    break;
+
+                default:
+                    startDate = new DateTime();
+                    endDate = new DateTime();
+                    break;
+            }
+
+            for (DateTime currentDate = startDate; currentDate <= endDate; currentDate = currentDate.AddDays(1))
+            {
+                week.Add(currentDate);
+            }
+
+            return week;
         }
     }
 }
--- LexiconLMS/Models/SchedulePost.cs
+++ LexiconLMS/Models/SchedulePost.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +8,15 @@ namespace LexiconLMS.Models
 {
     public class SchedulePost
     {
+        [DisplayName("Datum")]
         public string Date { get; set; }
+        [DisplayName("Dag")]
         public string Day { get; set; }
+        [DisplayName("Modul")]
         public string Module { get; set; }
+        [DisplayName("Förmiddag")]
         public ICollection<ScheduleLink> Morning { get; set; }
+        [DisplayName("Eftermiddag")]
         public ICollection<ScheduleLink> Afternoon { get; set; }
     }
 }
