a822dd675f7fbe4b8e8826010bfcf41f2cd8e17a
Mikael Åberg <mikael.aberg@me.com>
2017-03-30 16:50:39 +0200

    Change redirect from index to list

--- LexiconLMS/Controllers/DocumentsController.cs
+++ LexiconLMS/Controllers/DocumentsController.cs
@@ -124,7 +124,6 @@ namespace LexiconLMS.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "Id,Name,Description,UserId,CourseId,ModuleId,ActivityId")] Document document, HttpPostedFileBase file)
         {
-
             if (ModelState.IsValid)
             {
                 try
@@ -151,7 +150,7 @@ namespace LexiconLMS.Controllers
                         document.UploadTime = DateTime.Now;
                         db.Documents.Add(document);
                         db.SaveChanges();
-                        return RedirectToAction("Index");
+                        return RedirectToAction("List");
                     }
 
                 }
